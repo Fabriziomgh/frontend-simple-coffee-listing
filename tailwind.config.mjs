d4a3bd24d838285bd3d845e5bbd9117eae895713
Fabrizio Gutierrez <gutierrezfabrizio03@gmail.com>
2024-05-04 13:59:52 -0430

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+   theme: {
+      colors: {
+         'heading-color': '#FEF7EE',
+      },
+      extend: {},
+   },
+   plugins: [],
+};
